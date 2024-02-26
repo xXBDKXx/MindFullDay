f4b86ede0cda36cceaa599263165e076c39a2294
xXBDKXx <brunodantegames@gmail.com>
2024-02-26 14:50:41 -0300

    NavBar

--- lib/util/navbar.dart
+++ lib/util/navbar.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:mindfullday_v1/login_page.dart';
 
 class NavBar extends StatelessWidget {
   const NavBar({super.key});
@@ -18,9 +18,18 @@ class NavBar extends StatelessWidget {
             currentAccountPicture: CircleAvatar(
               child: ClipOval(),
             ),
-            ),
-          ],
-        ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => LoginPage()),
+              );
+            },
+            child: Text('Logout'),
+          ),
+        ],
+      ),
     );
   }
 }
