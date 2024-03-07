3e35e13913a2893ef990165894b481ad5cfc6a76
xXBDKXx <brunodantegames@gmail.com>
2024-03-07 17:13:29 -0300

    Small Changes

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -15,101 +15,109 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     const logo = "assets/imagens/logo.png";
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: Color.fromRGBO(134, 150, 254, 1),
-      body: SafeArea(
-        child: Center(
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/imagens/bg-login.png'),
-                opacity: 0.5,
-                fit: BoxFit.fill,
-              ),
-            ),
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 50,
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color.fromRGBO(134, 150, 254, 1),
+        body: ListView(
+          children: [
+            ConstrainedBox(
+              constraints: BoxConstraints(
+                  //MediaQuery e um metodo de pegar o tamanho do dispositivo
+                  maxHeight: MediaQuery.of(context).size.height,
+                  maxWidth: MediaQuery.of(context).size.width,
                 ),
-                Column(
-                  children: [
-                    const Image(
-                      image: AssetImage(logo),
-                      width: 266,
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-                Campotexto(
-                  controller: nomeController,
-                  hintText: 'Nome',
-                  obscureText: false,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Campotexto(
-                    controller: senhaController,
-                    hintText: 'Senha',
-                    obscureText: true
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/imagens/bg-login.png'),
+                    opacity: 0.5,
+                    fit: BoxFit.fill,
                   ),
-                  SizedBox(height: 8),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Esqueceu sua senha?',
-                        style: TextStyle(color: const Color.fromARGB(255, 255, 255, 255)),
+                ),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    Column(
+                      children: [
+                        const Image(
+                          image: AssetImage(logo),
+                          width: 266,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Campotexto(
+                      controller: nomeController,
+                      hintText: 'Nome',
+                      obscureText: false,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Campotexto(
+                        controller: senhaController,
+                        hintText: 'Senha',
+                        obscureText: true
                       ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => Home()),
-                    );
-                  },
-                  child: Text('Entrar'),
-                ),
-                SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Não é cadastrado?',
-                      style: TextStyle(color: Color.fromARGB(255, 255, 255, 255))
+                      SizedBox(height: 8),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Esqueceu sua senha?',
+                            style: TextStyle(color: const Color.fromARGB(255, 255, 255, 255)),
+                          ),
+                        ],
+                      ),
                     ),
-                    const SizedBox(width: 4,),
-                    GestureDetector(
-                      onTap: () {
+                    SizedBox(
+                      height: 8,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
                         Navigator.pushReplacement(
                           context,
-                          MaterialPageRoute(builder: (context) => Cadastro()),
+                          MaterialPageRoute(builder: (context) => Home()),
                         );
                       },
-                      child: Text(
-                        'Cadastre-se agora!',
-                        style: TextStyle(
-                          color: const Color.fromARGB(255, 72, 33, 243),
-                          fontWeight: FontWeight.bold,
+                      child: Text('Entrar'),
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Não é cadastrado?',
+                          style: TextStyle(color: Color.fromARGB(255, 255, 255, 255))
                         ),
-                      ),
+                        const SizedBox(width: 4,),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(builder: (context) => Cadastro()),
+                            );
+                          },
+                          child: Text(
+                            'Cadastre-se agora!',
+                            style: TextStyle(
+                              color: const Color.fromARGB(255, 72, 33, 243),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ],
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
--- lib/paginas/cadastro.dart
+++ lib/paginas/cadastro.dart
@@ -16,102 +16,111 @@ class Cadastro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     const logo = "assets/imagens/logo.png";
-    return Scaffold(
-      
-      resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.deepPurple,
-      body: SafeArea(
-        child: Center(
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/imagens/bg-login.png'),
-                fit: BoxFit.cover,
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.deepPurple,
+        body: ListView(
+          children: [
+            ConstrainedBox(
+              constraints: BoxConstraints(
+                  //MediaQuery e um metodo de pegar o tamanho do dispositivo
+                  maxHeight: MediaQuery.of(context).size.height,
+                  maxWidth: MediaQuery.of(context).size.width,
+                ),
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/imagens/bg-login.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Center(
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      Column(
+                        children: [
+                          const Image(
+                            image: AssetImage(logo),
+                            width: 266,
+                          ),
+                          Text(
+                            'Cadastre-se!!',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Campotexto(
+                        controller: nomeController,
+                        hintText: 'Nome',
+                        obscureText: false,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Campotexto(
+                          controller: senhaController,
+                          hintText: 'Senha',
+                          obscureText: true
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Campotexto(
+                          controller: teleController,
+                          hintText: 'Telefone',
+                          obscureText: false
+                        ),
+                      SizedBox(
+                        height: 10
+                      ),
+                      Campotexto(
+                          controller: emailController,
+                          hintText: 'Email',
+                          obscureText: false
+                        ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(builder: (context) => LoginPage()),
+                              );
+                            },
+                            child: Text('Cadastrar'),
+                          ),
+                          SizedBox(width: 10),
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(builder: (context) => LoginPage()),
+                              );
+                            },
+                            child: Text('Voltar'),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                    ],
+                  ),
+                ),
               ),
             ),
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 50,
-                ),
-                Column(
-                  children: [
-                    const Image(
-                      image: AssetImage(logo),
-                      width: 266,
-                    ),
-                    Text(
-                      'Cadastre-se!!',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-                Campotexto(
-                  controller: nomeController,
-                  hintText: 'Nome',
-                  obscureText: false,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Campotexto(
-                    controller: senhaController,
-                    hintText: 'Senha',
-                    obscureText: true
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Campotexto(
-                    controller: teleController,
-                    hintText: 'Telefone',
-                    obscureText: false
-                  ),
-                SizedBox(
-                  height: 10
-                ),
-                Campotexto(
-                    controller: emailController,
-                    hintText: 'Email',
-                    obscureText: false
-                  ),
-                SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(builder: (context) => LoginPage()),
-                        );
-                      },
-                      child: Text('Cadastrar'),
-                    ),
-                    SizedBox(width: 10),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(builder: (context) => LoginPage()),
-                        );
-                      },
-                      child: Text('Voltar'),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 10),
-              ],
-            ),
-          ),
+          ],
         ),
       ),
     );
